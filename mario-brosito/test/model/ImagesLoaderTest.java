9bf0cfb4f6a56d309c30e91b4f1b320fc7b68101
Christian Flor <christian.flor1@correo.icesi.edu.co>
2019-05-08 23:54:16 -0500

    Add Claas and package necesary for the project

---
+++ mario-brosito/test/model/ImagesLoaderTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ImagesLoaderTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ mario-brosito/test/model/LevelTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LevelTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
