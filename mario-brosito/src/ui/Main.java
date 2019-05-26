4491f8baa4bd3276e819412fd7c132bef9dcef7c
Ana Muñoz <munoz.am.118@gmail.com>
2019-05-25 21:40:17 -0500

    new image background for initial menu

--- mario-brosito/src/ui/Main.java
+++ mario-brosito/src/ui/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application{
 		Parent root = loader.load();
 	
 		Scene scene = new Scene(root);
-
+		
 		stage.setScene(scene);
 		stage.setTitle("Mariosito");
 		stage.show();
